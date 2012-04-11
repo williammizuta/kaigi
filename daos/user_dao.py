1a63c91c941be9974affae582ad4dd0cbae50aa4
William Seiti Mizuta <williammizuta@gmail.com>
2012-04-10 22:01:03 -0300

    Adding logic to create an admin user

--- daos/user_dao.py
+++ daos/user_dao.py
@@ -1,5 +1,12 @@
+from google.appengine.ext import db
+
 from models.user import User
 
 class UserDAO:
-	def insert(user):
-		User.put(self, user)
+	def insert_admin(self, google_user):
+		admin = User(user=google_user, status = db.Category('ADMIN'))
+		User.put(admin)
+
+	def load(self, google_user):
+		users = User.all()
+		return users.filter("user = ", google_user).get()
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 from google.appengine.ext import db
 
 class User(db.Model):
-    user = db.UserProperty(required=True)
+    user = db.UserProperty(auto_current_user_add=True, required=True)
     status = db.CategoryProperty(default='PENDING', choices=['ADMIN', 'PENDING', 'APPROVED', 'DECLINED'], required=True)
