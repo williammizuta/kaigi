6af0474b6e7f1df517ae261ae015c69e8463974b
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2012-04-10 21:52:20 -0300

    Changing to Python 2.7

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-import logging
 import os
-
-import tornado.web
 import tornado.wsgi
-from tornado.web import url
-
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
-
-import forms
 import handlers.index
 
 from daos.meeting_dao import MeetingDAO
@@ -16,22 +7,13 @@ from daos.meeting_dao import MeetingDAO
 # Constants
 IS_DEV = os.environ['SERVER_SOFTWARE'].startswith('Dev')  # Development server
 
-class Application(tornado.wsgi.WSGIApplication):
-    def __init__(self):
-        routes = [
-                (r'/', handlers.index.IndexHandler, dict(meetings=MeetingDAO())),
-                ]
-        settings = dict(
-                static_path=os.path.join(os.path.dirname(__file__), "static"),
-                template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-                xsrf_cookies=True,
-                cookie_secret="asjidoh91239jasdasdasdasdasdkja8izxc21312sjdhsa/Vo=",
-                )
-        tornado.wsgi.WSGIApplication.__init__(self, routes, **settings)
+settings = dict(
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+    xsrf_cookies=True,
+    cookie_secret="asjidoh91239jasdasdasdasdasdkja8izxc21312sjdhsa/Vo=",
+    )
 
-
-def main():
-    run_wsgi_app(Application())
-
-if __name__ == '__main__':
-    main()
+application = tornado.wsgi.WSGIApplication([
+    (r'/', handlers.index.IndexHandler, dict(meetings=MeetingDAO())),
+    ], **settings)
