84ddddc1d1f15321280da8d124bfbdfd6f77bdcd
William Seiti Mizuta <william.mizuta@gmail.com>
2012-04-13 11:38:18 -0300

    Small refactoring in user decorator to verify if an user is approved or not

--- decorators/user.py
+++ decorators/user.py
@@ -3,27 +3,11 @@ import tornado.web
 
 from daos.user_dao import UserDAO
 
-def user(method):
+def approved(method):
+    @tornado.web.authenticated
     @functools.wraps(method)
     def wrapper(self, *args, **kargs):
-
-        def put_next_url(url):
-            if "?" not in url:
-                if urlparse.urlsplit(url).scheme:
-                    next_url = self.request.full_url()
-                else:
-                    next_url = self.request.uri
-                url += "?" + urllib.urlencode(dict(next=next_url))
-            return url
-
         google_user = self.get_current_user()
-        if not google_user:
-            if self.request.method in ("GET", "HEAD"):
-                complete_url = put_next_url(self.get_login_url())
-                return self.redirect(complete_url)
-            raise HTTPError(403)
-            return 
-
         user = UserDAO().load(google_user)
 
         if user is None:
--- handlers/dashboard.py
+++ handlers/dashboard.py
@@ -1,12 +1,12 @@
 import tornado.web
 
 from handlers.base import BaseHandler
-from decorators.user import user
+import decorators.user
 
 class DashboardHandler(BaseHandler):
     def initialize(self, user_dao):
         self.user_dao = user_dao
 
-    @user
+    @decorators.user.approved
     def get(self):
         self.render('dashboard.html')
