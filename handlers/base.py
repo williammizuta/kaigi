a91d0b0b026156fbf821940589949398304428c3
William Seiti Mizuta <william.mizuta@gmail.com>
2012-04-14 03:21:48 -0300

    Adding kaigi object to all templates

--- handlers/base.py
+++ handlers/base.py
@@ -1,18 +1,17 @@
 import tornado.web
 from google.appengine.api import users
+from daos.kaigi_dao import KaigiDAO
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         return users.get_current_user()
 
     def render_string(self, template_name, **kwargs):
-        return tornado.web.RequestHandler.render_string(self, template_name, users=users, **kwargs)
+        kaigi = KaigiDAO().get()
+        return tornado.web.RequestHandler.render_string(self, template_name, users=users, kaigi=kaigi, **kwargs)
 
     def get_login_url(self):
         return users.create_login_url(self.request.uri)
 
     def get_approve_pending_url(self):
         return "/pending"
-
-    def get_subscribe_url(self):
-        return "/subscribe"
