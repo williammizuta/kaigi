3fa01ebbe2accb92ead3c9db330b2eca54d58fec
William Seiti Mizuta <william.mizuta@gmail.com>
2012-04-14 03:27:52 -0300

    Fixing home handler test

--- test/handlers/home_test.py
+++ test/handlers/home_test.py
@@ -6,8 +6,7 @@ class home_handler_test:
     def setup(self):
         self.handler = object.__new__(HomeHandler)
         self.user_dao = mock()
-        self.kaigi_dao = mock()
-        self.handler.initialize(self.user_dao, self.kaigi_dao)
+        self.handler.initialize(self.user_dao)
         when(self.handler).redirect().thenReturn(None)
         when(self.handler).render().thenReturn(None)
 
@@ -19,12 +18,10 @@ class home_handler_test:
     def should_render_home_page_if_no_user_is_logged_in_but_there_are_registered_users(self):
         when(self.user_dao).has_no_user().thenReturn(False)
         when(self.handler).get_current_user().thenReturn(None)
-        existing_kaigi = mock()
-        when(self.kaigi_dao).get().thenReturn(existing_kaigi)
 
         self.handler.get()
 
-        verify(self.handler).render('home.html', kaigi=existing_kaigi)
+        verify(self.handler).render('home.html')
 
     def should_redirect_to_dashboard_if_there_is_a_registered_user_and_there_is_a_logged_user(self):
         logged_user = mock()
